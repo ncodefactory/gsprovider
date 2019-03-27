a68d3da5a9187a0f1e64b378e8252de876e6d28b
Bartosz Solarski <bartosz.solarski@hotmail.com>
2019-03-27 11:34:02 +0100

    For cs environment settings updated

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import { gsstatus } from './gsprovider';
 
 (async () => {
   try {
-    const statusReder = gsstatus('192.168.1.147', 9000);
+    const statusReder = gsstatus('192.168.2.88', 9000);
     const status = await statusReder(1);
     console.log(status); // eslint-disable-line no-console
   } catch (error) {
